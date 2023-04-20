bfe291e466117654e214526f971c13865cb42488
Andreas Schneider <xichtus@MacBook-Pro-von-Andreas.fritz.box>
2023-04-20 15:56:42 +0200

    done

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,40 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe('Schnitzel',
+            'This is simply a schnitzel with fries',
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [
+                new Ingredient('Schnitzel', 1),
+                new Ingredient('French Fries', 20),
+                new Ingredient('Lemon Slice', 1)
+            ]),
+        new Recipe('A big fat Burger',
+            'This is simply a burger',
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [
+                new Ingredient('MeatPatty', 1),
+                new Ingredient('Buns', 1),
+                new Ingredient('Cheese', 1)
+            ])
     ];
+
+    constructor(private shoppingListService: ShoppingListService) {
+        
+    }
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
     }
 }
